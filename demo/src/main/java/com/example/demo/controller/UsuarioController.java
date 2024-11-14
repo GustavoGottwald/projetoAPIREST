f2ca68624e7d2ddcf8606c4e4e0898c40778daee
Gustavo Gottwald <gustavogottwald@outlook.com>
2024-11-13 21:15:55 -0300

    Primeiro commit do projeto

---
+++ demo/src/main/java/com/example/demo/controller/UsuarioController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Usuario;
+import com.example.demo.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @PostMapping
+    public Usuario criarUsuario(@RequestBody Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @GetMapping
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
+    }
+}
