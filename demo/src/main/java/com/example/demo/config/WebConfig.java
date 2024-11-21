1be8a9bd8716da8afcd08e95b61152ad86a4c5ff
Gustavo Gottwald <gustavogottwald@outlook.com>
2024-11-20 23:24:34 -0300

    alguns comentarios sobre padroes de projetos

--- demo/src/main/java/com/example/demo/config/WebConfig.java
+++ demo/src/main/java/com/example/demo/config/WebConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// A classe WebConfig geralmente contém configurações que devem ser compartilhadas em todo o projeto, como configuração de CORS
+// ao marcar a classe com anotações como @Configuration, ela automaticamente se comportará como um Singleton
+
 @SuppressWarnings("unused")
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
--- demo/src/main/java/com/example/demo/controller/UsuarioController.java
+++ demo/src/main/java/com/example/demo/controller/UsuarioController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// Facade é usado para fornecer uma interface simplificada para um conjunto de classes 
+// ou subsistemas complexos, escondendo a complexidade dos detalhes interno
+// O padrão Facade pode ser identificado na camada de serviços, 
+// principalmente onde os controladores interagem com a lógica de negócios que nesse projeto é aqui
+
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
